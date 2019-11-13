edc9167e4e1c2c7b96cb9f90ba90d14e44c85853
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-11-13 11:14:00 -0800

    added tests for case-sensitivity, removed extra blank lines

--- test/is_pangram.test.js
+++ test/is_pangram.test.js
@@ -7,51 +7,50 @@ describe('isPangram()', () => {
 
   test('works with a pangram with only lower case', () => {
     const text = 'the quick brown fox jumps over the lazy dog';
-
     const result = isPangram(text);
-
     expect(result).toBe(true);
   });
 
   test('works with "abcdefghijklmnopqrstuvwxyz"', () => {
     const text = 'abcdefghijklmnopqrstuvwxyz';
-
     const result = isPangram(text);
-
     expect(result).toBe(true);
   });
 
   test("fails if missing character 'x'", () => {
     const textx = 'abcdefghijklmnopqrstuvwyz';
-
     const resultx = isPangram(textx);
-
     expect(resultx).toBe(false);
   });
 
   test('fails if empty sentence', () => {
     const text = '';
-
     const result = isPangram(text);
-
     expect(result).toBe(false);
   });
 
   test('works for pangram with underscores instead of spaces', () => {
     const text = 'the_quick_brown_fox_jumps_over_the_lazy_dog';
-
     const result = isPangram(text);
-
     expect(result).toBe(true);
   });
 
   test('works for pangram with numbers', () => {
     const text = 'the 5 quick brown foxes jump over the 3 lazy dogs';
-
     const result = isPangram(text);
-
     expect(result).toBe(true);
   });
 
   // Write your own test case
+  test('works for mixed-case pangram', () => {
+    const text = 'AbcdefghijklmnopqrstuvwxyZ';
+    const result = isPangram(text);
+    expect(result).toBe(true);
+  });
+
+  test('works for upper-case pangram', () => {
+    const text = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase();
+    const result = isPangram(text);
+    expect(result).toBe(true);
+  });
 });
